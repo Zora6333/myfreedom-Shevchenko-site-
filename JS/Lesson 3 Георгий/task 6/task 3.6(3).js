f6de869d7ab305a1945412c428a3fec42d88de2e
Georgei <georgeisevchenko@gmail.com>
2023-02-19 16:56:36 +0300

    studing js

--- JS/Lesson 3 Георгий/task 6/task 3.6(3).js
+++ JS/Lesson 3 Георгий/task 6/task 3.6(3).js
@@ -1,14 +1,24 @@
 
 let anyNumber = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
-var maximum = Math.max.apply(Math, anyNumber);
-var empty = [];
 
-for (i = 0; i < anyNumber.length; i++) {
-  
-  if (anyNumber[i] === maximum) {
+let i = 0;
+let empty = [];
+let maximum = 0;
 
+while (i < anyNumber.length) {
+  if (anyNumber[i] > maximum) {
+    maximum = anyNumber[i]
+  }
+
+  i++;
+} for (i = 0; i < anyNumber.length; i++) {
+
+
+  if (anyNumber[i] == maximum) {
     empty.push(i);
   }
 
 }
-console.log("Позиции (индексы) максимального числа в массиве: " + empty);
+alert("Позиции (индексы) максимального числа в массиве: " + empty);
+
+alert("Максимальное число массива: " + maximum);
