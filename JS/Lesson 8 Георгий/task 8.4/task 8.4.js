ca5a38624dda5439ab64ee963bbaea210da74d0b
Georgei <georgeisevchenko@gmail.com>
2023-02-20 00:29:02 +0300

    studing js

---
+++ JS/Lesson 8 Георгий/task 8.4/task 8.4.js
@@ -0,0 +1,12 @@
+let but = document.getElementById('but1')
+
+ 
+ window.addEventListener("load",function() { changeBackground('red') });
+
+function changeBackground(color) {
+    
+    if(document.getElementById('but2').style.display='block'){
+    document.body.style.background = color;} else{
+        none
+    }
+ }
